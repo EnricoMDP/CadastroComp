d6973e4b442488b3bc757ddca59771aed20d7af5
Enrico Meira <enrico.depaulameira@gmail.com>
2024-12-07 16:57:27 -0300

    Entrega de IHC

---
+++ Data/CadastroCompContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CadastroComp.Models;
+
+namespace CadastroComp.Data
+{
+    public class CadastroCompContext : DbContext
+    {
+        public CadastroCompContext (DbContextOptions<CadastroCompContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroComp.Models.Cliente> Cliente { get; set; } = default!;
+    }
+}
